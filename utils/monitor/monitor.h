f8edb3c8d51d515c7399dc76622fa2a740c78ab1
NicolasDenoyelle <denoyelle.nicolas@gmail.com>
2015-01-22 16:42:38 +0100

    add function to replace or add watched pid

--- utils/monitor/monitor.h
+++ utils/monitor/monitor.h
@@ -50,37 +50,18 @@ struct monitors{
 typedef struct monitors * Monitors_t;
 
 
-Monitors_t new_default_Monitors             (hwloc_topology_t topology,
-					     const char * output, 
-					       unsigned int pid);
-Monitors_t load_Monitors                    (hwloc_topology_t topology,
-					     const char * perf_group_file, 
-					       const char * output, 
-					       unsigned int pid);
-
-int        Monitors_start                   (Monitors_t m);
-
-void       Monitors_update_counters         (Monitors_t m);
-void       Monitors_wait_update             (Monitors_t m);
-
-long long  Monitors_get_counter_value       (Monitors_t m,
-					       unsigned int counter_idx,
-					       unsigned int PU_idx);
-double     Monitors_get_monitor_max         (Monitors_t m, 
-					       unsigned int m_idx);
-double     Monitors_get_monitor_min         (Monitors_t m, 
-					       unsigned int m_idx);
-double     Monitors_get_monitor_value       (Monitors_t m, 
-					       unsigned int node_idx, 
-					       unsigned int PU_idx);
-double     Monitors_get_monitor_variation   (Monitors_t m, 
-					       unsigned int m_idx, 
-					       unsigned int sibling_idx);
-double     Monitors_wait_monitor_value      (Monitors_t m, 
-					       unsigned int m_idx, 
-					       unsigned int sibling_idx);
-double     Monitors_wait_monitor_variation  (Monitors_t m, 
-					       unsigned int m_idx, 
-					       unsigned int sibling_idx);
-void       delete_Monitors                  (Monitors_t m);
+Monitors_t   new_default_Monitors             (hwloc_topology_t topology, const char * output, unsigned int pid);
+Monitors_t   load_Monitors                    (hwloc_topology_t topology, const char * perf_group_file, const char * output, unsigned int pid);
+unsigned int Monitors_watch_pid               (Monitors_t m,unsigned int pid);
+int          Monitors_start                   (Monitors_t m);
+void         Monitors_update_counters         (Monitors_t m);
+void         Monitors_wait_update             (Monitors_t m);
+long long    Monitors_get_counter_value       (Monitors_t m, unsigned int counter_idx, unsigned int PU_idx);
+double       Monitors_get_monitor_max         (Monitors_t m, unsigned int m_idx);
+double       Monitors_get_monitor_min         (Monitors_t m, unsigned int m_idx);
+double       Monitors_get_monitor_value       (Monitors_t m, unsigned int node_idx, unsigned int PU_idx);
+double       Monitors_get_monitor_variation   (Monitors_t m, unsigned int m_idx, unsigned int sibling_idx);
+double       Monitors_wait_monitor_value      (Monitors_t m, unsigned int m_idx, unsigned int sibling_idx);
+double       Monitors_wait_monitor_variation  (Monitors_t m, unsigned int m_idx, unsigned int sibling_idx);
+void         delete_Monitors                  (Monitors_t m);
 #endif
